0e04ab087e8f9b586f6d5851324bcaa9d6b1c8d9
jrhe1213 <jiarong1213@gmail.com>
2022-06-14 18:51:03 -0400

    add dummy middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DemoService } from './demo/demo.service';
 import { DemoController } from './demo/demo.controller';
+// middleware
+import { InitMiddleware } from './middleware/init.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController, DemoController],
   providers: [AppService, DemoService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // apply to all routes
+    consumer.apply(InitMiddleware).forRoutes('*');
+  }
+}
---
+++ src/middleware/init.middleware.ts
@@ -0,0 +1,12 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+
+/**
+ * cli: nest g middleware middleware/init
+ */
+@Injectable()
+export class InitMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+    console.log('this is dummy middleware');
+    next();
+  }
+}
