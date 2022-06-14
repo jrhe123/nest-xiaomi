69375dd9fca018abb78018895fd2d2666fe49022
jrhe1213 <jiarong1213@gmail.com>
2022-06-14 18:06:22 -0400

    demo controller add notes

--- src/demo/demo.controller.ts
+++ src/demo/demo.controller.ts
@@ -1,6 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Query,
+  Param,
+  Body,
+  Headers,
+  Session,
+  Next,
+  Request,
+  Response,
+} from '@nestjs/common';
 import { DemoService } from './demo.service';
 
+/**
+ * route:
+ * :id -> Param() -> req.params
+ * query -> req.query
+ * body -> req.body
+ * headers -> req.headers
+ * session -> req.session
+ * next()
+ */
 @Controller('demo')
 export class DemoController {
   constructor(private demoServices: DemoService) {}
@@ -10,5 +31,20 @@ export class DemoController {
     return {
       list: this.demoServices.findAll(),
     };
+  }
+
+  @Post()
+  create(@Request() req, @Response() res) {
+    // 1. cookie usage
+    // const signedCookies = req.signedCookies;
+    // res.cookie('username', 'roy', {
+    //   maxAge: 1000 * 60 * 60,
+    //   httpOnly: true,
+    // });
+    // 2. session usages
+    req.session.username = 'abc';
+    res.json({
+      msg: req.session.username,
+    });
   }
 }
