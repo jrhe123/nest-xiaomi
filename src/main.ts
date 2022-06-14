6b7e9d88b09bf69682db2d386c2f3510deab4147
jrhe1213 <jiarong1213@gmail.com>
2022-06-14 17:49:14 -0400

    add session middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import * as path from 'path';
+//
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // express app
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as path from 'path';
 import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,11 +14,20 @@ async function bootstrap() {
   app.useStaticAssets(path.join(__dirname, '..', 'public'), {
     prefix: '/static/',
   });
-  // 2. html viewer
+  // 2. html template
   app.setBaseViewsDir(path.join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
-  // 3. cookie parser
-  app.use(cookieParser);
+  // 3. cookie middleware
+  app.use(cookieParser('my super secret password'));
+  // 4. session middleware
+  app.use(
+    session({
+      secret: 'another secret here',
+      resave: false,
+      saveUninitialized: true,
+      cookie: { secure: true },
+    }),
+  );
 
   await app.listen(3030);
 }
