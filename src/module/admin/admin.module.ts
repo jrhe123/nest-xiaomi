8852e9092f1df0fd4970135266781553805979d2
jrhe1213 <jiarong1213@gmail.com>
2022-06-17 18:27:24 -0400

    add admin module

--- src/module/admin/admin.module.ts
+++ src/module/admin/admin.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { LoginController } from './login/login.controller';
+import { MainController } from './main/main.controller';
+import { ManagerController } from './manager/manager.controller';
 
-@Module({})
+@Module({
+  controllers: [LoginController, MainController, ManagerController],
+})
 export class AdminModule {}
