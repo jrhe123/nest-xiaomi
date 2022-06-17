2d803de65f5479ccb9fc381d6fd317453b1da587
jrhe1213 <jiarong1213@gmail.com>
2022-06-17 18:39:23 -0400

    add global service tools class

--- src/module/admin/admin.module.ts
+++ src/module/admin/admin.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+// controllers
 import { MainController } from './main/main.controller';
 import { LoginController } from './login/login.controller';
 import { ManageController } from './manage/manage.controller';
+// services
+import { ToolsService } from 'src/service/tools/tools.service';
 
 @Module({
   controllers: [MainController, LoginController, ManageController],
+  providers: [ToolsService],
 })
 export class AdminModule {}
