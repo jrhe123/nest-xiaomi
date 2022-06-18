d7fe50ec275a13f4464ce6594aae25de8e7c0d6c
jrhe1213 <jiarong1213@gmail.com>
2022-06-17 20:33:01 -0400

    ui change

--- src/module/admin/manage/manage.controller.ts
+++ src/module/admin/manage/manage.controller.ts
@@ -1,9 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller('admin/manage')
 export class ManageController {
   @Get()
+  @Render('admin/manage/index')
   index() {
-    return 'admin manage page';
+    return {};
+  }
+
+  @Get('add')
+  @Render('admin/manage/add')
+  add() {
+    return {};
+  }
+
+  @Get('edit')
+  @Render('admin/manage/edit')
+  edit() {
+    return {};
   }
 }
