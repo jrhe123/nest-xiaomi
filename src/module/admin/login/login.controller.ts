f101260300c88031c70a5e7c6c934e95ec80d492
jrhe1213 <jiarong1213@gmail.com>
2022-06-17 20:44:25 -0400

    add captcha svg

--- src/module/admin/login/login.controller.ts
+++ src/module/admin/login/login.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Request, Response } from '@nestjs/common';
+import { ToolsService } from 'src/service/tools/tools.service';
 
 @Controller('admin/login')
 export class LoginController {
+  constructor(private toolsService: ToolsService) {}
+
   @Get()
   @Render('admin/login')
   index() {
     return {};
+  }
+
+  @Get('code')
+  getCode(@Request() req, @Response() res) {
+    const captcha = this.toolsService.captcha();
+    req.session.code = captcha.text;
+    res.type('image/svg+xml');
+    res.send(captcha.data);
   }
 }
--- src/service/tools/tools.service.ts
+++ src/service/tools/tools.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+import * as svgCaptcha from 'svg-captcha';
+
 @Injectable()
-export class ToolsService {}
+export class ToolsService {
+  captcha() {
+    const captcha = svgCaptcha.create({
+      size: 4,
+      fontSize: 50,
+      width: 100,
+      height: 40,
+      background: '#cc9966',
+    });
+    return captcha;
+  }
+}
